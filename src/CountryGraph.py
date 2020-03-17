975d822a3b9c09bc6f0276e36320694d74c12e4e
DorenCalliku <dcalliku@gmail.com>
2020-03-17 20:30:04 +0300

    Updated map. Next update base model.

--- src/CountryGraph.py
+++ src/CountryGraph.py
@@ -1,12 +1,8 @@
-# computational needs
 import networkx as nx
 import pandas as pd
-# local files
-import roads
-# others
-from unidecode import unidecode
 import random, json
 import numpy as np, numpy.random
+from unidecode import unidecode
 
 class CountryGraph:
     """Creates a distribution of the cities spatially based on data."""
@@ -14,50 +10,61 @@ class CountryGraph:
     def __init__(self, country="Turkey"):
         self.country = country
         self.G = nx.Graph()
+        self.subs = {}
+        self.__update_cities()
+        self.__update_subs()
+        self.__update_roads()
+        self.__create_graph()
         
-    @static
+    @staticmethod
     def remove_duplicates(seq):
         seen = set()
         seen_add = seen.add
         return [x for x in seq if not (x in seen or seen_add(x))]
     
-    def create_graph(self):
-        """Use information from different sources to 
-        create a map of Turkey's cities."""
-
-        cities = pd.read_csv("cities_turkey.csv")
+    def __update_cities(self):
+        self.cities = pd.read_csv("geo_data/cities_"+ self.country.lower() +".csv")
         
-        # Put everything in a graph
-        G = nx.Graph()
-        # create each city
-        for city in list(cities['city']):
-            # find position of city in map
-            city_properties = cities[cities['city'] == city].iloc[0]
-            # add the city to the graph
-            G.add_node( city_properties['city'], 
+    def __update_roads(self):
+        with open("geo_data/roads_turkey") as f:
+            self.roads = list(json.loads( json.load( f)).values())
+        
+    def __update_subs(self):
+        # get the cities where the main roads pass from in turkey
+        cities_subs = pd.read_csv('geo_data/cities_subs.csv')
+        for e in range(len( cities_subs)):
+            self.subs[ cities_subs.iloc[e]['city'] ] = cities_subs.iloc[e]['admin']  
+        
+    def __create_graph(self):
+        """Update the graph of the country."""
+
+        for city in list(self.cities['city']):
+            city_properties = self.cities[self.cities['city'] == city].iloc[0]
+            self.G.add_node( city_properties['city'], 
                        pos  = ( city_properties['lat'], city_properties[ 'lng']),
                        size = int( city_properties['population']),
                        area = int( city_properties['Area(km²)'].replace(',','')),
                        unem = int( city_properties['Unemployment']))
-        # put the edges connecting the cities
-        # if found city in road, but not in data, fix manually
-        for road in main_roads:
-            # if any of the paths is not in the list, then mention it, fix it manually
-            for each in road:
-                if each not in list(cities['city']):
-                    print("City not found in list: ",each) # TODO: deal automatically
-            # remind the cities that are present
+        road_cities = []
+        for road in self.roads:
+            road = self.remove_duplicates([self.subs[i] for i in road])
+            road_cities += road # add the cities which are connected to this road
             for i in range(len(road)-1):
-                # add a connection between each two cities
-                G.add_edge( road[i], road[i+1], weight=10)
-        # connect the cities not in the list with the closest city in the list
-        not_found = set( cities['city'])- set(sum(main_roads,[]))
+                self.G.add_edge( road[i], road[i+1], weight=10)
+        road_cities = set(road_cities)
+        not_found = set( self.cities['city']) - set(road_cities)
         for each in not_found:
-            city = cities[ cities['city'] == each].iloc[0]
+            city = self.cities[ self.cities['city'] == each].iloc[0]
             # order cities present in list by distance to this city
-            found = cities[cities['city'].isin( all_cities)]
+            found = self.cities[ self.cities['city'].isin( road_cities)]
             found['distance'] = ((found['lat']-city['lat'])**2 + (found['lng']-city['lng'])**2)
-            closest_city = found.sort_values('distance').iloc[1]['city'] # get the closest
-            # add road connecting these cities
-            G.add_edge( city['city'], closest_city, weight=10)
-        return G
+            try:
+                closest_city = found.sort_values('distance').iloc[1]['city'] # get the closest
+                # add road connecting these cities
+                self.G.add_edge( city['city'], closest_city, weight=10)
+            except:
+                found.sort_values('distance')
+                
+    def display_graph(self):
+        pos = {city:(long, lat) for (city, (lat,long)) in nx.get_node_attributes(self.G, 'pos').items()}
+        nx.draw(self.G, pos, with_labels=False, node_size=10, node_color='r', edge_color='b')
